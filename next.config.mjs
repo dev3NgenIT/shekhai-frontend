86f846fbf064a38465ab84bc5cde19c627969b97
Szamansaju <szamansaju@gmail.com>
2026-01-26 14:03:03 +0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    images: {
-        domains: ["localhost"],
-      },
+  reactStrictMode: true,
+
+  images: {
+    domains: [
+      "localhost",
+      "shekhai-server.onrender.com", // your API server domain
+      "res.cloudinary.com",           // if you use Cloudinary
+      "images.unsplash.com"           // if you use Unsplash
+    ],
+  },
+
+  // Next.js default output is ".next" (do NOT change)
+  // distDir: "build"   <-- remove this
+
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
