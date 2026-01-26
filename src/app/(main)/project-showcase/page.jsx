855448b9124068d106bd632b09d8c470e73fa466
Szamansaju <szamansaju@gmail.com>
2026-01-26 13:55:06 +0600

    first commit

--- src/app/(main)/project-showcase/page.jsx
+++ src/app/(main)/project-showcase/page.jsx
@@ -1,9 +1,9 @@
 import Banner from "@/components/project-showcase/Banner";
-import CategoryProjects from "@/components/project-showcase/CategoryProjects/CategoryProjects";
-import ProjectCarousel from "@/components/project-showcase/ProjectCarousel";
-import ProjectCTA from "@/components/project-showcase/ProjectCTA/ProjectCTA";
-import Projects from "@/components/project-showcase/Projects";
-import ProjectsSection from "@/components/project-showcase/ProjectsSection/ProjectsSection";
+// import CategoryProjects from "@/components/project-showcase/CategoryProjects/CategoryProjects";
+// import ProjectCarousel from "@/components/project-showcase/ProjectCarousel";
+// import ProjectCTA from "@/components/project-showcase/ProjectCTA/ProjectCTA";
+// import Projects from "@/components/project-showcase/Projects";
+// import ProjectsSection from "@/components/project-showcase/ProjectsSection/ProjectsSection";
 
 export default function Page() {
   return (
@@ -11,16 +11,16 @@ export default function Page() {
       <div className="container-width mx-auto mt-4 px-4 md:mt-0">
         <Banner />
 
-        <ProjectCarousel />
+        {/* <ProjectCarousel /> */}
       </div>
 
       {/* <ProjectsSection /> */}
 
-      <CategoryProjects />
+      {/* <CategoryProjects /> */}
 
-      <ProjectCTA />
+      {/* <ProjectCTA /> */}
 
-      <Projects />
+      {/* <Projects /> */}
     </main>
   );
 }
